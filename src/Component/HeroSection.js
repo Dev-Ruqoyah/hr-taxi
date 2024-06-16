8f599e856a2d8d907cd5eea72c3236aa7cc38bc0
bossLady0001 <hamzatrukayatabiodun02@gmail.com>
2024-06-16 03:12:05 -0700

    first commit

---
+++ src/Component/HeroSection.js
@@ -0,0 +1,30 @@
+import React from "react";
+const HeroSection = () => {
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div className="flex">
+          <div className="basis-3/4">
+            <div className="hero-text">
+              <h3>
+                Book your comformtable and Hassle free ride at{" "}
+                <span>HR-TAXI</span>{" "}
+              </h3>
+              <div className="hero-image">
+                <img src="" alt="" />
+              </div>
+              {/* <Button>Book now</Button> */}
+            </div>
+          </div>
+          <div className="basis-1/4">
+          <div className="hero-text1">
+    
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/Component/navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Header = () => {
+    return ( 
+        <div className="bg-gray-500 p-3 text-2xl text-white">
+            Header
+        </div>
+     );
+}
+ 
+export default Header;
